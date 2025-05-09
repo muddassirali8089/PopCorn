0305c341d9efd193f85c37fd585d04de8b6c6cc7
muddassirali8089 <114619571+muddassirali8089@users.noreply.github.com>
2025-05-09 23:25:51 +0500

    Add the loading state to the api
    Add the loading state to the api

--- PopCorn/src/App.jsx
+++ PopCorn/src/App.jsx
@@ -56,17 +56,23 @@ export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState(tempWatchedData);
 
+  const [isLoading , setIsLoading] = useState(false);
+
   useEffect(function () {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
       );
       const data = await res.json();
+      
      setMovies(data.Search);
+     setIsLoading(false);
       
     }
     fetchMovies();
   }, []);
+
 
   return (
     <>
@@ -75,7 +81,7 @@ export default function App() {
       </NavBar>
       <Main>
         <Box>
-          <MovieList movies={movies} />
+          {isLoading ? <Loader/> :<MovieList movies={movies} />}
         </Box>
 
         <Box>
@@ -87,6 +93,13 @@ export default function App() {
   );
 }
 
+
+function Loader(){
+
+  return(
+    <p className="loader"> Loading....</p>
+  )
+}
 function NavBar({ children }) {
   return (
     <nav className="nav-bar">
